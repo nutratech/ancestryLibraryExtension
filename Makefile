3396d7e9753e42d66a263dce9f8365ecd751d4b8
Shane Jaroch <chown_tee@proton.me>
2025-06-18 15:57:58 -0400

    working v0.1.0

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
 SHELL := /bin/bash
 
+.DEFAULT_GOAL := build
+
 .PHONY: format
 format:
 	prettier -w .
+
+
+.PHONY: build
+build:
+	zip -rvl9 ext_packed_ancestry_lib.zip \
+	ancestryLibrary.js manifest.json icons/
--- ancestryLibrary.js
+++ ancestryLibrary.js
@@ -7,14 +7,23 @@ browser.contextMenus.create(
     contexts: ["link"],
   },
   // onCreated,
-  // () => void browser.runtime.lastError,
+  () => void browser.runtime.lastError,
 );
 
-// browser.contextMenus.onClicked.addListener((info, tab) => {
-//   switch (info.menuItemId) {
-//     case "open-link-in-ancestry-library":
-//       console.log(info.selectionText);
-//       break;
-//     // ...
-//   }
-// });
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+  console.log(info);
+  switch (info.menuItemId) {
+    case "open-link-in-ancestry-library":
+      console.log(info.selectionText);
+      console.info("Open in AL: " + info.linkUrl);
+      if (info.linkUrl) {
+        let newTab = browser.tabs.create({
+          active: false,
+          url: info.linkUrl.replace("ancestry.com", "ancestrylibrary.com"),
+          index: tab.index + 1,
+        });
+      }
+      break;
+    // ...
+  }
+});
